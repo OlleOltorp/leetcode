cda03323fa3d31ca4f0d871ce3771b9c750a83c7
Olle Oltorp <olle.oltorp@live.se>
2026-08-20 15:27:31 +0200

    217

--- neetcode/1arrays&hashing/217.java
+++ neetcode/1arrays&hashing/217.java
@@ -1,9 +1,19 @@
-// 217. Check array for duplicates
+// 217. Contains Duplicate
 // https://leetcode.com/problems/contains-duplicate/
-// Idea: 
+// Time: O(n) Space: O(n)
+
+import java.util.Set;
+import java.util.HashSet;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
 
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- random/1232/Solution.java
+++ random/1232/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
 
-        int[] coord1 = coordinates[0];
-        int[] coord2 = coordinates[1];
+        int[] cord1 = coordinates[0];
+        int[] cord2 = coordinates[1];
 
-        int v0 = coord2[0] - coord1[0];
-        int v1 = coord2[1] - coord1[1];
+        int v0 = cord2[0] - cord1[0];
+        int v1 = cord2[1] - cord1[1];
 
         for (int i = 2; i < coordinates.length; i++) {
             int[] coordinate = coordinates[i];
 
-            int t0 = coordinate[0] - coord1[0];
-            int t1 = coordinate[1] - coord1[1];
+            int t0 = coordinate[0] - cord1[0];
+            int t1 = coordinate[1] - cord1[1];
 
             if (((v0 * t1) - (v1 * t0)) != 0) {
                 return false;
