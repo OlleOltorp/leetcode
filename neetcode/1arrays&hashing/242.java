9def3aaf5df32f07dd7a95843b9e6413a7c0fa8d
Olle Oltorp <olle.oltorp@live.se>
2026-08-20 16:23:43 +0200

    242

---
+++ neetcode/1arrays&hashing/242.java
@@ -0,0 +1,29 @@
+// 242. Valid Anagram
+// https://leetcode.com/problems/valid-anagram/description/
+// Time: O(n) Space: O(1)
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] arrayCounter = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int a = s.charAt(i) - 'a';
+            int b = t.charAt(i) - 'a';
+            arrayCounter[a]++;
+            arrayCounter[b]--;
+        }
+
+        for (int i = 0; i < arrayCounter.length; i++) {
+            if (arrayCounter[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
