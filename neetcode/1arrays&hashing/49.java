e2b4952e15b3bbfe91975348c487600c82b76e96
Olle Oltorp <olle.oltorp@live.se>
2026-08-20 22:57:30 +0200

    49

---
+++ neetcode/1arrays&hashing/49.java
@@ -0,0 +1,38 @@
+// 49. Group Anagrams
+// https://leetcode.com/problems/group-anagrams/
+// Time: O(n*k) Space: O(n*k)
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            String s = strs[i];
+            int[] charCounter = new int[26];
+
+            for (int j = 0; j < s.length(); j++) {
+                int a = s.charAt(j) - 'a';
+                charCounter[a]++;
+            }
+
+            String identifier = Arrays.toString(charCounter);
+
+            List<String> anagrams = anagramMap.get(identifier);
+            if (anagrams == null) {
+                List<String> anagramList = new ArrayList<>();
+                anagramList.add(s);
+                anagramMap.put(identifier, anagramList);
+                continue;
+            }
+            anagrams.add(s);
+        }
+
+        return new ArrayList<>(anagramMap.values());
+    }
+}
