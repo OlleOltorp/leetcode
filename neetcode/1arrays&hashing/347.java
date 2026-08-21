07e7753d9fd09a8426eb93eaa15b0a66c8a01bf3
Olle Oltorp <olle.oltorp@live.se>
2026-08-21 12:43:38 +0200

    347

--- neetcode/1arrays&hashing/347.java
+++ neetcode/1arrays&hashing/347.java
@@ -1,14 +1,57 @@
 // 347. Top K Frequent Elements
 // https://leetcode.com/problems/top-k-frequent-elements/
-// Time: O() Space: O()
+// Time: O(n) Space: O(n)
 
-// Notes: 
-// three passes. first insert all onto map (counts)
-// insert all into list with int, lists where the index is the count 
-// go through  this from the back until we have k entries
+// Idea: 
+// Three passes
+// First pass counts
+// Second pass creates sorted index array
+// Third pass takes out largest values (starting at the end)
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> seenMap = new HashMap<>();
 
+        for (int i = 0; i < nums.length; i++) {
+            seenMap.merge(nums[i], 1, (a, b) -> a + b);
+        }
+
+        List<Integer>[] indexArray = new ArrayList[nums.length + 1];
+
+        for (Map.Entry<Integer, Integer> numEntry : seenMap.entrySet()) {
+            int key = numEntry.getKey();
+            int val = numEntry.getValue();
+
+            List<Integer> list = indexArray[val];
+            if (list == null) {
+                list = new ArrayList<>();
+                indexArray[val] = list;
+            }
+            list.add(key);
+        }
+
+        int[] returnList = new int[k];
+        int currentFree = k - 1;
+        for (int i = indexArray.length - 1; i >= 0; i--) {
+            if (currentFree == -1) {
+                return returnList;
+            }
+            List<Integer> list = indexArray[i];
+
+            if (list == null) {
+                continue;
+            }
+
+            for (Integer integer : list) {
+                returnList[currentFree] = integer;
+                currentFree--;
+            }
+        }
+        return null;
     }
 }
--- neetcode/1arrays&hashing/problems.md
+++ neetcode/1arrays&hashing/problems.md
@@ -4,7 +4,7 @@
 - [x] 242 - Valid Anagram - Easy
 - [x] 1 - Two Sum - Easy
 - [x] 49 - Group Anagrams - Medium
-- [ ] 347 - Top K Frequent Elements - Medium
+- [x] 347 - Top K Frequent Elements - Medium
 - [ ] 271 - Encode and Decode Strings - Medium (Premium)
 - [ ] 238 - Product of Array Except Self - Medium
 - [ ] 36 - Valid Sudoku - Medium
